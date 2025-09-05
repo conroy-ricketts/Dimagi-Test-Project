a5d8634ddf282fc17eb268496ec212fd044c75df
Conroy Ricketts <conroy.ricketts@tutanota.com>
2025-09-05 11:26:39 -0400

    Tweaked the UI a bit to center the submit button and fill the entire screen's width with the chat history.

--- app/src/main/java/com/example/myapplication/features/chatscreen/ChatScreen.kt
+++ app/src/main/java/com/example/myapplication/features/chatscreen/ChatScreen.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -20,6 +21,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -51,7 +53,7 @@ fun ChatScreen(context: Context = LocalContext.current) {
 fun InputTextBox(viewModel: ChatViewModel) {
     var text by remember { mutableStateOf("") }
 
-    Row {
+    Row(verticalAlignment = Alignment.CenterVertically) {
         OutlinedTextField(
             value = text,
             onValueChange = { newText -> text = newText },
@@ -72,7 +74,7 @@ fun InputTextBox(viewModel: ChatViewModel) {
 fun ChatHistory(viewModel: ChatViewModel) {
     val chatHistory by viewModel.chatHistory.observeAsState(emptyList())
 
-    LazyColumn {
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
         items(chatHistory) {  message ->
             Text(text = message)
             Spacer(modifier = Modifier.height(10.dp))
